616c1638664cbe324cf01822f18b71326c8e7cc6
Ayush Auti <ayushauti5605@gmail.com>
2026-09-15 10:43:39 +0530

    done with adaIn layer

--- NST_Code/utils/utils.py
+++ NST_Code/utils/utils.py
@@ -40,7 +40,14 @@ def get_transform(size,crop,final_size):
 
 def adaptive_instance_normalization(content_feat,style_feat):
     #(batch_size,channels(s,h,w))
-    pass
+    size=content_feat.size()
+    style_mean,style_std=calc_mean_std(style_feat)
+    content_mean,content_std=calc_mean_std(content_feat)
+    normalized_content_feat=(content_feat-content_mean.expand(size))/content_std.expand(size)
+
+    return normalized_content_feat * style_std.expand(size)*style_mean.expand(size)
+
+
 
 def calc_mean_std(feat,eps=1e-5):
     #batch_size,channels(s,h,w)
